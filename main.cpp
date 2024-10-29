bafca35f813621375f976b91d6cc263a683a569e
MarcBaleiron <mbaleand@myuax.com>
2024-10-29 11:15:06 +0100

    Ej terminado

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+    int triples[4][3] =
+    {
+        {3, 4, 5},
+        {6, 8, 10},
+        {5, 12, 13},
+        {8, 15, 17}
+    };
+
+    cout << "Triples pitagoricos encontrados:\n";
+    for (int i = 0; i < 4; i++)
+    {
+        int a = triples[i][0];
+        int b = triples[i][1];
+        int c = triples[i][2];
+
+        if ((a^2 + b^2 == c^2) && (a + b > c) && (a + c > b) && (b + c > a))
+        {
+            cout << a << " - " << b << " - " << c << endl;
+        }
+
+    }
+        return 0;
+}
